1d8f2e9214d7156f70c4d01b94bf2a908d7a9e47
Lumin <luminubc@student.ubc.ca>
2026-06-09 22:51:30 -0700

    Update streamlitUI.py

--- streamlitUI.py
+++ streamlitUI.py
@@ -524,11 +524,11 @@ with st.sidebar:
 
     # ADDED: default to the first level-0 node so the center still has content with no search yet
     if st.session_state.path_ids:
-    root_id = st.session_state.path_ids[0]
-    root_rows = fetch_root_subtree(root_id)
-    indexes = build_subtree_indexes(root_rows, root_id)
-    st.session_state.root_indexes = indexes
-    render_clickable_path(st.session_state.path_ids, indexes)
+        root_id = st.session_state.path_ids[0]
+        root_rows = fetch_root_subtree(root_id)
+        indexes = build_subtree_indexes(root_rows, root_id)
+        st.session_state.root_indexes = indexes
+        render_clickable_path(st.session_state.path_ids, indexes)
 
     st.divider()
     st.subheader("Bill of materials")
